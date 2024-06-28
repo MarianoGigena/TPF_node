7fddbe10d3b114ff7cd6499ca38b89c8b9357b01
MarianoGigena <marianogigena@gmail.com>
2024-06-28 01:49:31 -0300

    deploy en render

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';//se agrego dotenv
 dotenv.config();
 
 const dbPassword = process.env.SECRET_KEY;
+const dbPassword2 = process.env.SECRET_KEY2;
 const pool = createPool({
     host: 'b3zynustzghx1t94uama-mysql.services.clever-cloud.com',
     user: 'uyy5ny5uxvgfphu0',
@@ -12,6 +13,16 @@ const pool = createPool({
     waitForConnections: true,
     connectionLimit: 5
 });
+
+/* const pool = createPool({
+    host: 'mysql-nodetpf.alwaysdata.net',
+    user: 'nodetpf',
+    password: dbPassword2,
+    database: 'nodetpf_demousuarios',
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 5
+}); */
 
 pool.getConnection()
     .then(connection => {
