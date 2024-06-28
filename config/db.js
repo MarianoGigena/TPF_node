ab8b3d7eeb242d213d625620c02d84d666d97420
MarianoGigena <marianogigena@gmail.com>
2024-06-27 23:41:10 -0300

    variable de entorno OK

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const dbPassword = process.env.SECRET_KEY;
 const pool = createPool({
     host: 'b3zynustzghx1t94uama-mysql.services.clever-cloud.com',
     user: 'uyy5ny5uxvgfphu0',
-    //password: 'IShvMQ2znUuPv8l0PFot',
     password: dbPassword,
     database: 'b3zynustzghx1t94uama',
     port: 3306,
